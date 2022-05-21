2562952d9c860845dbb4e50f9e785324d157bf85
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-05-21 14:18:23 +0200

    install webpack encor

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,21 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+        .setPublicPath('/build')
+        // only needed for CDN's or sub-directory deploy
+        //.setManifestKeyPrefix('build/')
+         .copyFiles({
+                 from: './assets/images',
+        
+                // optional target path, relative to the output dir
+                 to: 'images/[path][name].[ext]',
+        
+                 // if versioning is enabled, add the file hash too
+                 //to: 'images/[path][name].[hash:8].[ext]',
+        
+                // only copy files matching this pattern
+                //pattern: /\.(png|jpg|jpeg)$/
+            })
 
     /*
      * ENTRY CONFIG
@@ -56,7 +68,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
