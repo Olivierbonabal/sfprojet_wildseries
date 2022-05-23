756aeddec21b67e932f8df76ee7257298496548c
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-05-23 13:30:51 +0200

    challenge manytoone

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -25,6 +25,10 @@ class Program
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private $poster;
 
+    #[ORM\ManyToOne(targetEntity: Category::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -74,6 +78,18 @@ class Program
     public function setPoster(?string $poster): self
     {
         $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
